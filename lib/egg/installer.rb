7cbe082f6379ed0214664cc913a7de7944da1f61
Alex Deem <alex.deem@tabcorp.com.au>
2014-08-19 21:38:30 +1000

    Add Support for Frameworks

--- lib/egg/installer.rb
+++ lib/egg/installer.rb
@@ -50,6 +50,7 @@ class Installer
     project.build_configuration_list.set_setting("ONLY_ACTIVE_ARCH","NO")
 
     includePaths = []
+    frameworks = []
 
     libGroup = project.new_group('lib');
     @installedEggs.values.each { |dependency|
@@ -62,32 +63,37 @@ class Installer
       # Add the dep target as a dependency of the main target
       target.add_dependency(depTarget);
 
-      # Link the target lib to the main target
-      containerItemProxy = project.new(Xcodeproj::Project::PBXContainerItemProxy)
-      containerItemProxy.container_portal = depProjectReference.uuid
-      containerItemProxy.proxy_type = '2'
-      containerItemProxy.remote_global_id_string = depTarget.product_reference.uuid
-      containerItemProxy.remote_info = dependency.name
+      if depTarget.product_reference.explicit_file_type == "wrapper.framework"
+        # If it's a framework, we need to instruct the application to link it
+        frameworks << depTarget.product_reference.path.gsub(/\.framework\Z/,"")
+      else
+        # Link the target lib to the main target
+        containerItemProxy = project.new(Xcodeproj::Project::PBXContainerItemProxy)
+        containerItemProxy.container_portal = depProjectReference.uuid
+        containerItemProxy.proxy_type = '2'
+        containerItemProxy.remote_global_id_string = depTarget.product_reference.uuid
+        containerItemProxy.remote_info = dependency.name
 
-      referenceProxy = project.new(Xcodeproj::Project::PBXReferenceProxy)
-      referenceProxy.file_type = depTarget.product_reference.explicit_file_type
-      referenceProxy.path = depTarget.product_reference.path
-      referenceProxy.remote_ref = containerItemProxy
-      referenceProxy.source_tree = depTarget.product_reference.source_tree
+        referenceProxy = project.new(Xcodeproj::Project::PBXReferenceProxy)
+        referenceProxy.file_type = depTarget.product_reference.explicit_file_type
+        referenceProxy.path = depTarget.product_reference.path
+        referenceProxy.remote_ref = containerItemProxy
+        referenceProxy.source_tree = depTarget.product_reference.source_tree
 
-      productGroup = project.new(Xcodeproj::Project::PBXGroup)
-      productGroup.name = "Products"
-      productGroup.source_tree = "<group>"
-      productGroup << referenceProxy
+        productGroup = project.new(Xcodeproj::Project::PBXGroup)
+        productGroup.name = "Products"
+        productGroup.source_tree = "<group>"
+        productGroup << referenceProxy
 
-      attrb = Xcodeproj::Project::Object::AbstractObjectAttribute.new(:references_by_keys, :project_references, Xcodeproj::Project::PBXProject)
-      attrb.classes = [Xcodeproj::Project::PBXFileReference, Xcodeproj::Project::PBXGroup]
-      projectReference = Xcodeproj::Project::ObjectDictionary.new(attrb, project.root_object)
-      projectReference['ProjectRef'] = depProjectReference
-      projectReference['ProductGroup'] = productGroup
-      project.root_object.project_references << projectReference
+        attrb = Xcodeproj::Project::Object::AbstractObjectAttribute.new(:references_by_keys, :project_references, Xcodeproj::Project::PBXProject)
+        attrb.classes = [Xcodeproj::Project::PBXFileReference, Xcodeproj::Project::PBXGroup]
+        projectReference = Xcodeproj::Project::ObjectDictionary.new(attrb, project.root_object)
+        projectReference['ProjectRef'] = depProjectReference
+        projectReference['ProductGroup'] = productGroup
+        project.root_object.project_references << projectReference
 
-      target.frameworks_build_phase.add_file_reference(referenceProxy)
+        target.frameworks_build_phase.add_file_reference(referenceProxy)
+      end
     }
 
     # Add an xcconfig file for inclusion
@@ -97,6 +103,16 @@ class Installer
         f.write(" \"#{path}\"")
       }
       f.write("\n")
+
+      f.write("EGG_OTHER_LDFLAGS =")
+      frameworks.each { |framework|
+        f.write(" -framework \"#{framework}\"")
+      }
+      f.write("\n")
+
+      if frameworks.count > 0
+        f.write("EGG_FRAMEWORK_SEARCH_PATHS = $(CONFIGURATION_BUILD_DIR)")
+      end
     }
 
     project.save()
